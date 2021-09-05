ea97a57e0489ae50c201a5b0ed2c4678be7cb1db
Leland Takamine <leland.takamine@gmail.com>
2021-09-04 19:53:49 -0700

    Clean up generated project

---
+++ dadb/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.30"
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
