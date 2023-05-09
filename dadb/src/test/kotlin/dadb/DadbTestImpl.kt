fe0879b0365e33db04e4a9e8e5e338d5b78de018
Alexander H. Salgado <alexander.salgado@otrum.com>
2023-05-09 12:04:30 +0200

    Allow setting connect and socket timeouts when creating Dadb object

--- dadb/src/test/kotlin/dadb/DadbTestImpl.kt
+++ dadb/src/test/kotlin/dadb/DadbTestImpl.kt
@@ -1,6 +1,9 @@
 package dadb
 
+import org.junit.jupiter.api.Test
 import java.net.Socket
+import kotlin.test.assertFails
+import kotlin.test.assertNotNull
 
 internal class DadbTestImpl : DadbTest() {
 
@@ -22,4 +25,39 @@ internal class DadbTestImpl : DadbTest() {
 
         override fun close() = connection.close()
     }
+
+    @Test
+    fun validDefaultConstructorValues() {
+        val dadb = Dadb.create("localhost", 5555)
+        assertNotNull(dadb, "Unable to create Dadb object with default constructor values")
+    }
+
+
+    @Test
+    fun validConstructorValues() {
+        val dadb = Dadb.create("localhost", 5555, connectTimeout = 1000, socketTimeout = 10000)
+        assertNotNull(dadb, "Unable to create Dadb object with valid constructor values")
+    }
+
+    @Test
+    fun invalidPortConstructorValue() {
+        assertFails("Invalid port value was not validated") {
+            Dadb.create("localhost", -1, connectTimeout = 0, socketTimeout = 0)
+        }
+    }
+
+    @Test
+    fun invalidconnectTimeoutConstructorValue() {
+        assertFails("Invalid connectTimeout value was not validated") {
+            Dadb.create("localhost", 5555, connectTimeout = -1, socketTimeout = 0)
+        }
+    }
+
+    @Test
+    fun invalidSocketTimeoutConstructorValue() {
+        assertFails("Invalid socketTimeout value was not validated") {
+            Dadb.create("localhost", 5555, connectTimeout = 0, socketTimeout = -1)
+        }
+    }
+
 }
