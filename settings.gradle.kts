8410d5a1b5c300a7ea109fa9f0ffcef6ce55ec19
Bartek Pacia <barpac02@gmail.com>
2024-09-06 13:57:50 +0200

    Revert "clean up and upgrade Gradle files"
    This reverts commit 3dd68aa0213d925f21d766068c92bfffad5c4669.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,2 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "dadb"
 include("dadb")
