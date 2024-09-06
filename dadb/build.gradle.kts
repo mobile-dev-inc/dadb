7ee2fbabfd0bfc28eb493130b77cf5459e6a3e51
Bartek Pacia <barpac02@gmail.com>
2024-09-06 12:32:32 +0200

    bump vulnerable dependencies

--- dadb/build.gradle.kts
+++ dadb/build.gradle.kts
@@ -18,12 +18,12 @@ repositories {
 }
 
 dependencies {
-    api("com.squareup.okio:okio:2.10.0")
+    api("com.squareup.okio:okio:3.4.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("com.google.truth:truth:1.0.1")
+    testImplementation("com.google.truth:truth:1.4.4")
 }
 
 val toolchainFactory = objects.newInstance(JavaToolchainFactory::class.java)
