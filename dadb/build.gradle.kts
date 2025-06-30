f2691fcbbf5ec5ae09efe5c99239f0092190ab6c
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2025-07-01 04:20:01 +0530

    chore: update versions

--- dadb/build.gradle.kts
+++ dadb/build.gradle.kts
@@ -1,7 +1,6 @@
 import org.gradle.internal.jvm.inspection.DefaultJvmMetadataDetector
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.jvm.toolchain.internal.DefaultToolchainSpec
-import org.gradle.jvm.toolchain.internal.JavaToolchainFactory
 import org.gradle.jvm.toolchain.internal.JavaToolchainInput
 
 plugins {
@@ -26,7 +25,6 @@ dependencies {
     testImplementation("com.google.truth:truth:1.0.1")
 }
 
-val toolchainFactory = objects.newInstance(JavaToolchainFactory::class.java)
 val metadataDetector = objects.newInstance(DefaultJvmMetadataDetector::class.java)
 
 graal {
@@ -45,19 +43,6 @@ graalvmNative {
     binaries {
         all {
             buildArgs.add("-H:+EnableAllSecurityServices")
-            javaLauncher.set(provider {
-                val output = tasks.extractGraalTooling.get().outputs.files.singleFile
-                val javaHome = if (OperatingSystem.current().isMacOsX) output.resolve("Contents/Home") else output
-                val metadata = metadataDetector.getMetadata(javaHome)
-                if (!metadata.isValidInstallation) {
-                    throw RuntimeException(metadata.errorMessage)
-                }
-                val spec = objects.newInstance(DefaultToolchainSpec::class.java).apply {
-                    languageVersion.set(JavaLanguageVersion.of(java.targetCompatibility.majorVersion))
-                }
-                val javaToolchain = toolchainFactory.newInstance(javaHome, JavaToolchainInput(spec))
-                javaToolchain.get().javaLauncher
-            })
         }
     }
 }
