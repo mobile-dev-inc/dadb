40c20b171bfc2060b2770e7e2e12e0b8aa0c24c5
amanjeetsingh150 <amanjeetsingh150@gmail.com>
2025-10-17 18:19:00 +0530

    fix: update okio deps

--- dadb/build.gradle.kts
+++ dadb/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.internal.jvm.inspection.DefaultJvmMetadataDetector
-
 plugins {
     `maven-publish`
     id("org.jetbrains.kotlin.jvm")
@@ -12,15 +10,14 @@ repositories {
 }
 
 dependencies {
-    api("com.squareup.okio:okio:2.10.0")
+    api("com.squareup.okio:okio:3.16.2")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("com.google.truth:truth:1.0.1")
+    testImplementation("com.google.truth:truth:1.4.0")
 }
 
-val metadataDetector = objects.newInstance(DefaultJvmMetadataDetector::class.java)
 
 tasks.test {
     useJUnitPlatform()
